11d0740225474d427a8717c830f489823a13d812
Raphael Malka <raphael.malka@student.vinci.be>
2022-09-29 23:18:02 +0200

    myMovies : module 2 : dynamic homepage

--- exercises/module2/2.2/my-movie/src/index.js
+++ exercises/module2/2.2/my-movie/src/index.js
@@ -1,2 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+
+import ImageSpider from './img/spiderman.jpg'
+import ImageHulk from './img/hulk.jpg'
+
+
+renderSpidermanImage(ImageSpider);
+renderHulkImage(ImageHulk);
+renderTitle();
+
+function renderSpidermanImage(spiderURL){
+const image = new Image();
+image.src = spiderURL;
+image.height = 300;
+const div = document.getElementById('spiderman');
+div.appendChild(image);
+
+}
+
+function renderHulkImage(hulkURL){
+    const image = new Image();
+    image.src = hulkURL;
+    image.height = 300
+    const div = document.getElementById('hulk');
+    div.appendChild(image);
+}
+
+function renderTitle(){
+    const title = document.getElementById('title');
+    title.innerText = 'The new social network for movie lover'
+}
